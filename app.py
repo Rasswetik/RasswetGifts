5957cee091f23afe4dcf1efbb188134bc43cdec5
Raswetov <rasswetovyt@gmail.com>
2026-09-20 13:07:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template, render_template_string, request, jsonify, send_from_directory, redirect, make_response, g, has_request_context, send_file
 from jinja2 import TemplateNotFound
 import sqlite3
